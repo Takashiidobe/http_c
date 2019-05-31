534b51453c7642a0f6cf21d5b30e270342897321
takashiidobe <takashi.idobe@savvy.io>
2019-05-31 13:38:12 -0500

    added media

--- server.c
+++ server.c
@@ -1,4 +1,4 @@
-  // Server side C program to demonstrate Socket programming
+// Server side C program to demonstrate Socket programming
 #include <stdio.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -15,10 +15,8 @@ int main(int argc, char const *argv[])
   struct sockaddr_in address;
   int addrlen = sizeof(address);
   
-  char *file = "<html><body><h1>Hello World</h1></body></html>";
+  char *html = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 82\n\n<html><body><style>*{text-align:center;}</style><h1>Hello World</h1></body></html>";
 
-  char *hello = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 12\n\nHello world!";
-  char *error = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 46\n\n<html><body><h1>Hello World</h1></body></html>";
   // Creating socket file descriptor
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
   {
@@ -28,12 +26,11 @@ int main(int argc, char const *argv[])
 
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons( PORT );
+  address.sin_port = htons(PORT);
 
   memset(address.sin_zero, '\0', sizeof address.sin_zero);
 
-
-  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
   {
     perror("In bind");
     exit(EXIT_FAILURE);
@@ -43,19 +40,19 @@ int main(int argc, char const *argv[])
     perror("In listen");
     exit(EXIT_FAILURE);
   }
-  while(1)
+  while (1)
   {
     printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
     {
       perror("In accept");
       exit(EXIT_FAILURE);
     }
 
     char buffer[30000] = {0};
-    valread = read( new_socket , buffer, 30000);
-    printf("%s\n",buffer );
-    write(new_socket , error , strlen(error));
+    valread = read(new_socket, buffer, 30000);
+    printf("%s\n", buffer);
+    write(new_socket, html, strlen(html));
     printf("------------------Hello message sent-------------------\n");
     close(new_socket);
   }
